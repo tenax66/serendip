0d3d35c87fe301ca284acfb923fe10b7d436da14
esehara <esehara@gmail.com>
2023-04-02 21:46:49 +0900

    Discordに投稿するさい、タイトルと共にその項目の詳細を表示する (#1)
    * テスト用のオプションを作成する
    
    Botを外部から開発する場合、トークンが無いと文章がチェックできないのは
    大変なので、Botが投稿するであろう文章を生成するだけのflagを作る。
    
    * コマンドで文章を生成する部分を関数として切り離す
    
    * Wikipediaから項目の詳細を取ってきて、テキストに表示する
    
    * go fmtを走らせる
    
    * URLを展開しないように`<>`でURLを囲む
    
    * コマンドの文章がちょっとおかしいので変更
    
    * 検索結果を出来るだけ絞り込む
    
    rnlimitのようなパロメータが検索にもあるので、それを使って検索結果を絞り込む
    
    * 基礎的な部分で間違いがあったので、細かい修正を行う
    
    * 文章を組み立てるための関数を生成する
    
    恐らく事例としては考えにくいが、念のためにWikipediaのページに詳細が見つからない
    場合の分岐を作っている。
    
    このときに、出来るだけ`var`の宣言を減らしたいので、ifの内部で代入するよりは、
    関数化して、それぞれの分岐から文字列を返し、その結果を変数に代入したほうが、
    スッキリするかと思われるのでそのような形にした。
    
    * testOptの部分のカッコを外す
    
    * getDiscordMessegeを整理し、別途にgetSearchPageを分割する
    
    getRandomPage（ランダムに項目を拾ってくるメソッド）は存在するのに、
    項目から詳細を拾ってくる処理はベタ書きなのはアンバランスなので、
    二つの処理をそれぞれ関数としてわけて、スッキリさせる
    
    * 文字列の結合に`+=`を使うのではなく、`strings.Builder`を使う
    
    * 関数名をもう少しわかりやすい名前に変更する
    
    この関数の目的は、「検索で引っかかったページの単語の部分が`span`タグで囲まれている。
    このタグを削除する」というものなので、そういった処理であることを関数名で
    ある程度明示するようにした。
    
    * 明らかにいらない分岐処理を削除する
    
    これは`span`タグを消すのが主目的なので、`script`タグは出て来ないと
    判断し、この分岐は削除した。

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/tenax66/serendip
 
 go 1.20
 
-require github.com/bwmarrin/discordgo v0.27.1
+require (
+	github.com/bwmarrin/discordgo v0.27.1
+	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110
+)
 
 require (
 	github.com/gorilla/websocket v1.4.2 // indirect
--- main.go
+++ main.go
@@ -2,20 +2,31 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
+	"github.com/bwmarrin/discordgo"
+	"golang.org/x/net/html"
 	"log"
 	"net/http"
 	"net/url"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
-
-	"github.com/bwmarrin/discordgo"
 )
+
+var testOpt = flag.Bool("t", false, "テスト用です。投稿する文章をDiscordを経由せず表示します")
 
 const endpoint = "https://ja.wikipedia.org/w/api.php"
 
 func main() {
+	flag.Parse()
+	if *testOpt {
+		content, _ := getDiscordMessage()
+		fmt.Println(content)
+		return
+	}
+
 	TOKEN := os.Getenv("SERENDIP_BOT_TOKEN")
 
 	// DiscordのBot Tokenをセット
@@ -61,17 +72,65 @@ func main() {
 	dg.Close()
 }
 
+func paramsTemplate() url.Values {
+	params := url.Values{}
+	params.Set("action", "query")
+	params.Set("format", "json")
+	return params
+}
+
+func getWikipediaAPI(params url.Values) (*http.Response, error) {
+	// APIにリクエストを送信
+
+	resp, err := http.Get(endpoint + "?" + params.Encode())
+	if err != nil {
+		return resp, err
+	}
+
+	return resp, err
+}
+
+func makeContent(result WikipediaSearchResult, pageTitle string, pageURL string) (string, error) {
+	if len(result.Query.Search) == 0 {
+		return pageTitle + "\n" + "<" + pageURL + ">", nil
+	} else {
+		snippet := &result.Query.Search[0].Snippet
+		rawSnippet, err := html.Parse(strings.NewReader(*snippet))
+
+		if err != nil {
+			return "", err
+		}
+		pageDetail := removeTagFromText(rawSnippet)
+		return pageTitle + "\n\n" + pageDetail + "\n\n" + "<" + pageURL + ">", err
+	}
+}
+
+func getDiscordMessage() (string, error) {
+
+	pageTitle, pageURL, err := getRandomPage()
+
+	if err != nil {
+		log.Fatal("Error getting Wikipedia page: ", err)
+		return "", err
+	}
+
+	result, err := getSearchPage(pageTitle)
+	if err != nil {
+		log.Fatal("Error getting Wikipedia details: ", err)
+		return "", err
+	}
+
+	content, err := makeContent(result, pageTitle, pageURL)
+	return content, err
+}
+
 func onSlashCommand(s *discordgo.Session, i *discordgo.InteractionCreate) {
 	if i.ApplicationCommandData().Name == "wiki" {
 		// 取得したページのURLを返信
-		pageTitle, pageURL, err := getRandomPage()
+		content, err := getDiscordMessage()
 		if err != nil {
-			log.Println("Error getting Wikipedia page: ", err)
 			return
 		}
-
-		content := pageTitle + "\n" + pageURL
-
 		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 			Type: discordgo.InteractionResponseChannelMessageWithSource,
 			Data: &discordgo.InteractionResponseData{
@@ -84,22 +143,20 @@ func onSlashCommand(s *discordgo.Session, i *discordgo.InteractionCreate) {
 // Wikipediaのランダムなページを取得する関数
 func getRandomPage() (string, string, error) {
 	// リクエストパラメータを作成
-	params := url.Values{}
-	params.Set("action", "query")
-	params.Set("format", "json")
+	params := paramsTemplate()
 	params.Set("list", "random")
 	params.Set("rnnamespace", "0")
 	params.Set("rnlimit", "1")
 
-	// APIにリクエストを送信
-	resp, err := http.Get(endpoint + "?" + params.Encode())
+	resp, err := getWikipediaAPI(params)
+	defer resp.Body.Close()
+
 	if err != nil {
 		return "", "", err
 	}
-	defer resp.Body.Close()
 
 	// レスポンスをパース
-	var result WikipediaAPIResult
+	var result WikipediaRandomResult
 	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
 		return "", "", err
 	}
@@ -113,11 +170,49 @@ func getRandomPage() (string, string, error) {
 	return page.Title, "https://ja.wikipedia.org/wiki/" + url.PathEscape(page.Title), nil
 }
 
+func getSearchPage(pageTitle string) (WikipediaSearchResult, error) {
+	params := paramsTemplate()
+	params.Set("list", "search")
+	params.Set("srsearch", pageTitle)
+	params.Set("srlimit", "1")
+
+	resp, err := getWikipediaAPI(params)
+	defer resp.Body.Close()
+
+	var result WikipediaSearchResult
+	err = json.NewDecoder(resp.Body).Decode(&result)
+	return result, err
+}
+
+func removeTagFromText(n *html.Node) string {
+
+	if n.Type == html.TextNode {
+		return n.Data
+	}
+
+	var str_build strings.Builder
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		str_build.WriteString(removeTagFromText(c))
+	}
+
+	return strings.Join(strings.Fields(str_build.String()), " ")
+}
+
 // Wikipedia APIのレスポンスをパースするための型定義
-type WikipediaAPIResult struct {
+type WikipediaRandomResult struct {
 	Query struct {
 		Random []WikipediaPage `json:"random"`
 	} `json:"query"`
+}
+
+type WikipediaSearchResult struct {
+	Query struct {
+		Search []WikipediaSnippet `json:"search"`
+	} `json: "query"`
+}
+
+type WikipediaSnippet struct {
+	Snippet string `json:"snippet"`
 }
 
 // Wikipediaのページ情報を表す型定義
