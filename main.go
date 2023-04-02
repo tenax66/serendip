b122f46ada329e2ce6267f3cdb82f44efae91ed2
tenax66 <115792382+tenax66@users.noreply.github.com>
2023-04-02 10:18:12 +0900

    replace fmt print by log

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func main() {
 	// DiscordのBot Tokenをセット
 	dg, err := discordgo.New("Bot " + TOKEN)
 	if err != nil {
-		fmt.Println("Error creating Discord session: ", err)
+		log.Println("Error creating Discord session: ", err)
 		return
 	}
 
@@ -31,7 +31,7 @@ func main() {
 	// Discordに接続
 	err = dg.Open()
 	if err != nil {
-		fmt.Println("Error opening Discord session: ", err)
+		log.Println("Error opening Discord session: ", err)
 		return
 	}
 
@@ -46,11 +46,11 @@ func main() {
 
 	_, err = dg.ApplicationCommandBulkOverwrite(dg.State.User.ID, "", commands)
 	if err != nil {
-		fmt.Println("Error registering slash commands: ", err)
+		log.Println("Error registering slash commands: ", err)
 		return
 	}
 
-	fmt.Println("Bot is now running. Press CTRL-C to exit.")
+	log.Println("Bot is now running. Press CTRL-C to exit.")
 
 	// 終了シグナルを待機
 	sc := make(chan os.Signal, 1)
@@ -66,7 +66,7 @@ func onSlashCommand(s *discordgo.Session, i *discordgo.InteractionCreate) {
 		// 取得したページのURLを返信
 		pageTitle, pageURL, err := getRandomPage()
 		if err != nil {
-			log.Fatal("Error getting Wikipedia page: ", err)
+			log.Println("Error getting Wikipedia page: ", err)
 			return
 		}
 
